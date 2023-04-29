acf9bc95811e72c4b1364d9496a1d09ee30458c2
Luana Rodrigues Santos <l.rodrigues.santos@avanade.com>
2023-04-29 12:29:45 -0700

    subida do repositorio

---
+++ src/main/java/com/example/paymentservice/service/impl/PaymentServiceImpl.java
@@ -0,0 +1,28 @@
+package com.example.paymentservice.service.impl;
+
+import com.example.paymentservice.model.Payment;
+import com.example.paymentservice.service.PaymentService;
+import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+import java.io.Serializable;
+
+@RequiredArgsConstructor
+@Log4j2
+@Service
+public class PaymentServiceImpl implements PaymentService {
+
+    private final KafkaTemplate<String, Serializable> kafkaTemplate;
+
+    @SneakyThrows
+    @Override
+    public void sendPayment(Payment payment) {
+        log.info("PAYMENT_SERVICE_IMPL ::: Recebi o pagamento {}", payment);
+        Thread.sleep(1000);
+        log.info("Enviando pagamento...");
+        kafkaTemplate.send("payment-topic", payment);
+    }
+}
